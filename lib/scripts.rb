ed5ed9c66306b028a2d7283a11758e48fd12f2ab
Steven Anderson <sanderson@bpl.org>
2014-07-25 17:12:21 -0400

    Fix for test update

--- lib/scripts.rb
+++ lib/scripts.rb
@@ -379,20 +379,20 @@ class Scripts
 
     object_id_array = []
 
+=begin
     Bplmodels::ObjectBase.find_in_batches('has_model_ssim'=>"info:fedora/afmodel:Bplmodels_ObjectBase") do |group|
       group.each { |solr_object|
         object_id_array << solr_object['id']
       }
     end
+=end
 
 
-=begin
     ActiveFedora::Base.find_in_batches("*:*") do |group|
       group.each { |solr_object|
         object_id_array << solr_object['id']
       }
     end
-=end
 
     new_logger.error "Object array was: " + object_id_array.length.to_s
 
@@ -406,7 +406,6 @@ class Scripts
         main_object = ActiveFedora::Base.find(pid).adapt_to_cmodel
 
         #Fix test obhects....
-=begin
         if main_object.class.name == "ActiveFedora::Base" || main_object.class.name == "Bplmodels::SimpleObjectBase"
           main_object = main_object.adapt_to(Bplmodels::SimpleObjectBase)
 
@@ -444,7 +443,6 @@ class Scripts
           main_object = main_object.convert_to(instantiate_type)
           main_object.save
         end
-=end
 
         if main_object.relationships(:has_model).include?("info:fedora/afmodel:Bplmodels_File") || main_object.relationships(:has_model).include?("info:fedora/fedora-system:ContentModel-3.0") || main_object.relationships(:has_model).include?("info:fedora/afmodel:Bplmodels_Institution") || main_object.relationships(:has_model).include?("info:fedora/afmodel:Bplmodels_Collection")
 
@@ -511,9 +509,9 @@ class Scripts
               new_logger.error "No ARKS found for file object " + the_file.pid
             else
               ark_file_info = ark_file_info.first
-              # || the_file.productionMaster.label.include?('/'))
-              if(the_file.productionMaster.label == 'productionMaster datastream')
-                if ark_file_info.local_original_identifier.include?('/')
+              # )
+              if(the_file.productionMaster.label == 'productionMaster datastream'|| the_file.productionMaster.label.include?('/') || the_file.productionMaster.label.include?(' File'))
+                if ark_file_info.local_original_identifier.include?('/') || ark_file_info.local_original_identifier.include?(' File')
                   main_object.workflowMetadata.item_source.ingest_filepath.each do |item_source|
                     if item_source.include?(ark_file_info.local_original_identifier.gsub(' File', ''))
                       the_file.productionMaster.dsLabel = item_source.split('/').last.gsub('.tif', '').gsub('.jpg', '').gsub('.mp3', '').gsub('.wav', '').gsub('.pdf', '').gsub('.txt', '')
