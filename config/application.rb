0df18c7801e6edf58a422e3e87045d64ff205087
Steven Anderson <sanderson@bpl.org>
2014-08-15 15:20:30 -0400

    Added routes for resque

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module ArkHandler
   class Application < Rails::Application
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
     config.autoload_paths += %W(#{config.root}/lib)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
