e464b577ece400bad138a8191b4ea3c2c13d4f31
Steven Anderson <sanderson@bpl.org>
2015-08-13 13:41:22 -0400

    Hopefully fixed now to set secondary parent pids correctly

--- db/migrate/20150727173530_fix_secondary_parent_pids_to_arks.rb
+++ db/migrate/20150727173530_fix_secondary_parent_pids_to_arks.rb
@@ -1,4 +1,4 @@
-class AddSecondaryParentPidsToArks < ActiveRecord::Migration
+class FixSecondaryParentPidsToArks < ActiveRecord::Migration
   def change
     remove_column :arks, :secondary_parent_pids
     add_column :arks, :secondary_parent_pids, :string, array: true, default: '{}'
