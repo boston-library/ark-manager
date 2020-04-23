69aa20478f63b31d11c741c6a5a6dddf2e7b3ed6
code860 <benjaminc.barber@gmail.com>
2020-04-23 16:41:14 -0400

    Tweaked docker settings.
    modified entrypoint to add database.yml if it does not exist

--- Dockerfile
+++ Dockerfile
@@ -9,18 +9,16 @@ RUN apt-get update -qq && apt-get install -y postgresql-client
 RUN gem update --system
 RUN gem install bundler:2.1.2
 
-RUN mkdir /ark-manager
+RUN mkdir /ark-manager-app
 
-WORKDIR /ark-manager
+WORKDIR /ark-manager-app
 
-COPY Gemfile /ark-manager/Gemfile
-
-COPY Gemfile.lock /ark-manager/Gemfile.lock
+COPY Gemfile Gemfile.* /ark-manager-app/
 
 RUN bundle config build.nokogiri --use-system-libraries
 RUN bundle check || bundle install --jobs 3 --retry 3
 
-COPY . /ark-manager
+COPY . /ark-manager-app
 
 # Add a script to be executed every time the container starts.
 COPY entrypoint.sh /usr/bin/
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,17 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-mkdir -p /ark-manager/tmp/pids
+mkdir -p /ark-manager-app/tmp/pids
+rm -f /ark-manager-app/tmp/pids/server.pid
+rm -f /ark-manager-app/tmp/pids/server.state
 
-rm -f /ark-manager/tmp/pids/server.pid
-rm -f /ark-manager/tmp/pids/server.state
-rm -f /ark-manager/tmp/pids/puma.pid
-rm -f /ark-manager/tmp/pids/puma.state
+database_config_file=/ark-manager-app/config/database.yml
+if [ -f "$database_config_file" ]; then
+    echo "$database_config_file exist"
+else
+    cp /ark-manager-app/config/database.yml.sample $database_config_file
+fi
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
 
