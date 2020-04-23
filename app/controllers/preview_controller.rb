a55c28e87a3be43583bb1304cfad3857b56584fe
code860 <benjaminc.barber@gmail.com>
2020-04-23 15:52:54 -0400

    Removed load_project_configs initializers
    Added TODOs to previews controller

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -20,13 +20,15 @@ class PreviewController < ApplicationController
 
   # sends an image datastream to the client
   # for flagged items, return the image icon
+  # TODO: requires a service for retreiving from solr and azure blobs
+  #
   def return_image_datastream(datastream_id=nil, file_suffix)
     model = @ark.model_type
     filename = "#{@ark.pid}#{file_suffix}"
     if /File\z/ =~ model || /OAIObject\z/ =~ model
       datastream_url = image_url(@ark.pid, file_suffix, datastream_id)
     else
-      solr_response = SolrService.call(@ark.pid)
+      # solr_response = SolrService.call(@ark.pid)
       raise ImageNotFound, "No Exemplary Image Found In Solr" if solr_response['exemplary_image_ssi'].blank?
       if solr_response['flagged_content_ssi']
         send_icon(filename)
@@ -35,24 +37,23 @@ class PreviewController < ApplicationController
       end
     end
     if datastream_url
-      response = Typhoeus::Request.get(datastream_url)
-      raise ImageNotFound, "No Image Content Found at #{datastream_url}" if response.code == 404
-      send_image(filename, response.body)
+      # response = Typhoeus::Request.get(datastream_url)
+      # raise ImageNotFound, "No Image Content Found at #{datastream_url}" if response.code == 404
+      # send_image(filename, response.body)
     end
   end
 
   # returns a Fedora datastream url or IIIF url
   def image_url(pid, file_suffix, datastream_id)
-    return "#{IIIF_SERVER['url']}#{pid}/full/full/0/default.jpg" if file_suffix == '_full'
+    return "#{ENV['IIIF_SERVER_URL']}#{pid}/full/full/0/default.jpg" if file_suffix == '_full'
     datastream_disseminator_url(pid, datastream_id)
   end
 
   # returns the direct URL to a datastream in Fedora
   def datastream_disseminator_url(pid, datastream_id)
-    ActiveFedora::Base.connection_for_pid(pid).client.url + "/objects/#{pid}/datastreams/#{datastream_id}/content"
+    # TODO: pull in the image content from the azure blob its associated with.
+    # ActiveFedora::Base.connection_for_pid(pid).client.url + "/objects/#{pid}/datastreams/#{datastream_id}/content"
   end
-
-
 
   def send_icon(filename)
     send_file File.join(Rails.root, 'public', 'dc_image-icon.png'),
@@ -68,10 +69,8 @@ class PreviewController < ApplicationController
               :disposition => 'inline'
   end
 
-
   def find_ark
     @ark = Ark.find_by(noid: params[:noid])
     raise ActiveRecord::RecordNotFound, "Unable to locate ark with identifier-#{params[:noid]}" if @ark.blank?
   end
-
 end
