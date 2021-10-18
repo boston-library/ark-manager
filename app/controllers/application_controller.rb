6f80bf91bdcf05446387f4361e9fee21d2f3ef87
miao-bpl <rmiao@bpl.org>
2021-10-18 11:26:22 -0400

    Update author

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::API
   APP_INFO = {
     app_name: 'ark-manager',
-    author: 'Ben Barber Jr.',
+    author: 'Ben Barber Jr. Jr.',
     organization: 'Boston Public Library',
     version: '2',
   }.freeze
