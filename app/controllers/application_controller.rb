80fdf55e4311412f4a57aa11efab7455cabbef0b
miao-bpl <rmiao@bpl.org>
2023-01-06 16:08:59 -0500

    Test capistrano

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
   APP_INFO = {
     app_name: 'ark-manager',
     author: 'Ben Barber',
-    organization: 'Pittsfield Public Library',
+    organization: 'NYC Public Library',
     version: '2',
   }.freeze
 
