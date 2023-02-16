b1119b08f42cf7023c70c4d0d2011ea273da4ec4
miao-bpl <84741957+miao-bpl@users.noreply.github.com>
2023-02-16 12:29:42 -0500

    Capistrano (#65)
    * Add credentials from ENV variables
    
    * Add a list-releases task
    
    * add deploy/produciton.rb for eventual production deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,21 +2,18 @@
 
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.17.1'
-require File.expand_path('./environment', __dir__)
 
 set :use_sudo, false
 
-# If staging_case is set to "testing", capistrano deploys app to testing server.
-# switch :stage_case to "staging" when moving to staging enviroment
-set :stage_case, 'staging'
-# set :stage_case, 'testing'
+## STAGE_NAME is a paramter from Jenkins job: "staging", "qc", and "testing"
+set :stage_case, ENV['STAGE_NAME']
 
 set :application, 'ark-manager'
 set :repo_url, "https://github.com/boston-library/#{fetch(:application)}.git"
-set :user, Rails.application.credentials.dig("deploy_#{fetch(:stage_case)}".to_sym, :user)
+set :user, ENV['DEPLOY_USER']
 
 ## Make user home path dynamic.
-set :deploy_to, "/home/deployer/#{fetch(:application)}"
+set :deploy_to, "/home/#{fetch(:user)}/railsApps/#{fetch(:application)}"
 
 set :rvm_installed, "/home/#{fetch(:user)}/.rvm/bin/rvm"
 set :rvm_ruby_version, File.read(File.expand_path('./../.ruby-version', __dir__)).strip
@@ -26,7 +23,7 @@ set :rvm_bundle_version, File.read(File.expand_path('./Gemfile.lock'))[-10..-1].
 set :pty, true
 
 # Default value for linked_dirs is []
-append :linked_files, 'config/database.yml', 'config/credentials/staging.key', 'config/environments/staging.rb'
+append :linked_files, 'config/database.yml', 'config/credentials/staging.key', 'config/environments/staging.rb', 'config/credentials/production.key'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'bundle'
 
 # Default value for keep_releases is 5
@@ -71,6 +68,13 @@ namespace :boston_library do
       execute 'sudo /bin/systemctl reload nginx.service'
     end
   end
+
+  desc 'List current releases'
+  task :list_releases do
+    on roles(:app) do
+      execute "ls -alt #{fetch(:deploy_to)}/releases"
+    end
+  end
 end
 
 after :'bundler:config', :'boston_library:gem_update'
@@ -78,3 +82,4 @@ after :'boston_library:gem_update', :'boston_library:rvm_install_ruby'
 after :'boston_library:rvm_install_ruby', :'boston_library:install_bundler'
 after :'deploy:cleanup', :"boston_library:restart_#{fetch(:application)}_puma"
 after :"boston_library:restart_#{fetch(:application)}_puma", :'boston_library:restart_nginx'
+after :'boston_library:restart_nginx', :'boston_library:list_releases'
